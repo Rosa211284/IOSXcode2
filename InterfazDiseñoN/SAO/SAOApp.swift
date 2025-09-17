26f292bcbd1f4e8246ef1850fb98630a4367eae9
Rosa211284 <al211284@alumnos.uacj.mx>
2025-09-17 17:22:50 -0600

    recreacion de interfaz SAO

---
+++ InterfazDiseñoN/SAO/SAOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAOApp.swift
+//  SAO
+//
+//  Created by alumno on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SAOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
